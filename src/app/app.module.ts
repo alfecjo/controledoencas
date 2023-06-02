9ee4b022c9b28a45621d437511bab45573281fd0
Antonio Almeida <alfecjo@hotmail.com>
2023-06-02 20:17:47 -0300

    Atividade 08 - Criação do layout com componentes e uso do conceito de data-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { DisplayComponent } from './display/display.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
-  declarations: [AppComponent, CadastroComponent, DisplayComponent],
+  declarations: [
+    AppComponent,
+    CadastroComponent,
+    DisplayComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
